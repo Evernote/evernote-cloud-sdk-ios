49b439220fc5544cf03d9880b7e86e56d39e3e3d
Martin Pilkington <mpilkington@evernote.com>
2017-01-12 14:04:35 +0000

    Moved sample project to a separate project

---
+++ EvernoteSDKSample/EvernoteSDKSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EvernoteSDKSample
+//
+//  Created by Dylan Marriott on 12/01/17.
+//  Copyright © 2017 Evernote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
